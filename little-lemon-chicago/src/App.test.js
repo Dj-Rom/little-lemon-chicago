37685d5f833d526e44aad53cc414bb07e0c0dccc
Aleh Kapusta <alehkapusta@gmail.com>
2024-09-04 20:23:30 +0200

    Deploy React app to GitHub Pages

--- little-lemon-chicago/src/App.test.js
+++
@@ -1,8 +0,0 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
-
-test('renders appropriate text', () => {
-  render(<App />);
-  const linkElement = screen;
-  expect(linkElement);
-});
